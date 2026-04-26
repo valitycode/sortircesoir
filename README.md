0d2c6d617bec8d0915b578eff121b416e9aaac94
valitycode <marcantt2008@gmail.com>
2026-04-26 16:30:35 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+APP DE VALITY
+Application mobile qui permet de trouvé une soirée. 
+Plus d'info ? 
+Merci de me contacter
---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "sortircesoir-v4-1";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
